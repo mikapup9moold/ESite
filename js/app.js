084aad65a9b6aeac0e5fbb606fed415e754c8efc
M LOW <mlow991@users.noreply.github.com>
2016-02-06 02:04:48 -0800

    Menu items with ngrepeat

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 
 	app.controller('InventoryController', function($scope, $firebaseObject) {
 		var ref = new Firebase('https://blinding-heat-3195.firebaseio.com/menu');
-		$scope.data = $firebaseObject(ref);
+		var syncObject = $firebaseObject(ref);
+		syncObject.$bindTo($scope, "data");
 	});
 })();
