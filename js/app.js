c4db061de98e245b62579e5dbe2cf48566804925
M LOW <mlow991@users.noreply.github.com>
2016-02-05 20:58:54 -0800

    angu,a\\

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 (function() {
-	var app = angular.module('eStore', ['menu-directives']);
+	var app = angular.module('eStore', ['firebase']);
 
-	app.controller('InventoryController', function($scope, $http) {
-		$http.get('json/data.json').success(function(data) {
-			$scope.menu = data.menu;
-		});
+	app.controller('InventoryController', function($scope, $firebaseObject) {
+		var ref = new Firebase('https://blinding-heat-3195.firebaseio.com/menu');
+		$scope.data = $firebaseObject(ref);
 	});
 })();
