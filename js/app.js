5307cc454b492ba44adc6f1aa3eedfc3224dd545
M LOW <mlow991@users.noreply.github.com>
2016-05-31 23:42:03 -0700

    fix: Directory for login templates

--- js/app.js
+++ js/app.js
@@ -9,19 +9,19 @@
         $routeProvider
             .when('/', {
                 controller: 'HomeController',
-                templateUrl: 'home/home.view.html',
+                templateUrl: 'templates/home.view.html',
                 controllerAs: 'vm'
             })
 
             .when('/login', {
                 controller: 'LoginController',
-                templateUrl: 'login/login.view.html',
+                templateUrl: 'templates/login.view.html',
                 controllerAs: 'vm'
             })
 
             .when('/register', {
                 controller: 'RegisterController',
-                templateUrl: 'register/register.view.html',
+                templateUrl: 'templates/register.view.html',
                 controllerAs: 'vm'
             })
 
